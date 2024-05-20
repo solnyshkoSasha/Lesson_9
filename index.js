2a27017820567b3c5c92e8a607e0e3c0c6fc4ab2
solnyshkoSasha <158512299+solnyshkoSasha@users.noreply.github.com>
2024-05-20 13:34:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let user = prompt('Як ваші справи?', 'Okay');
+if (user === null) {
+  alert('Ви нічого не ввели!');
+} else {
+  console.log(user);
+}
+
+// let data = confirm('Чи все вам подобається?');
+// if (data) {
+//   alert('Привіт, як в тебе справи?');
+// } else {
+//   console.log('Error');
+// }
